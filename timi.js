bd542dd030dadabd67f76420afe6e0d91e21ff8e
marvsticks <marvsticks123@gmail.com>
2024-11-28 17:04:42 +0100

    gg

---
+++ timi.js
@@ -0,0 +1,10 @@
+// const shopIcons = document.querySelectorAll(".shop i")
+// shopIcons.forEach((i) => {
+//   i.addEventListener("mouseover", () => {
+//     i.classList.add("fa-2xl")
+//   })
+//   i.addEventListener("mouseout", () => {
+//     i.classList.remove("fa-2xl")
+
+//   })
+// })
